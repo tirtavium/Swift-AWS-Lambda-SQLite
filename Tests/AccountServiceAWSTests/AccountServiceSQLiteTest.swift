c0f7fc82bfaf0276f8ba656e1a4ad6f8c457b6d3
TirtaGunawan <tirta.gunawan@allianz.co.id>
2021-01-24 19:07:15 +0700

    disable unit test, due to bug on SPM

--- Tests/AccountServiceAWSTests/AccountServiceSQLiteTest.swift
+++ Tests/AccountServiceAWSTests/AccountServiceSQLiteTest.swift
@@ -1,27 +1,27 @@
+////
+////  File.swift
+////  
+////
+////  Created by Tirta Gunawan on 24/01/21.
+////
 //
-//  File.swift
-//  
-//
-//  Created by Tirta Gunawan on 24/01/21.
-//
-
-import Foundation
-import XCTest
-@testable import AccountServiceAWS
-/*
-Currently SPM unable to have unit test on executable project
-move it to Library type if you like to have unit test
-https://bugs.swift.org/browse/SR-9359
-*/
-final class AccountServiceSQLiteTest: XCTestCase {
-    
-    var service = AccountServiceSQLite()
-    
-    func testGetAccounts_Success(){
-        XCTAssertNotNil(service.getAccounts())
-    }
-    
-    func testFindAccount_Success(){
-        XCTAssertNotNil(service.findAccount(id: 1))
-    }
-}
+//import Foundation
+//import XCTest
+//@testable import AccountServiceAWS
+///*
+//Currently SPM unable to have unit test on executable project
+//move it to Library type if you like to have unit test
+//https://bugs.swift.org/browse/SR-9359
+//*/
+//final class AccountServiceSQLiteTest: XCTestCase {
+//    
+//    var service = AccountServiceSQLite()
+//    
+//    func testGetAccounts_Success(){
+//        XCTAssertNotNil(service.getAccounts())
+//    }
+//    
+//    func testFindAccount_Success(){
+//        XCTAssertNotNil(service.findAccount(id: 1))
+//    }
+//}
